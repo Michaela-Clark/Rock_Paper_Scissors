a0731d6b3781e035addc4d46587facd1cd786849
skeletonssnot <mclark13245@yahoo.com>
2026-02-24 17:34:36 -0500

    save work before merge

---
+++ Rock_Paper_Scissors/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rock_Paper_Scissors
+{
+    private static void DetermineWinner
+    {
+    if (player == computer)
+               {
+            Console.WriteLine();
+        }
+
+
+
+
+
+    }
+}
--- Rock_Paper_Scissors/obj/Debug/net10.0/Rock_Paper_Scissors.AssemblyInfo.cs
+++ Rock_Paper_Scissors/obj/Debug/net10.0/Rock_Paper_Scissors.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rock_Paper_Scissors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb46e6197f4b7769267c0ae6ab6365af213bc6aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rock_Paper_Scissors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rock_Paper_Scissors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
