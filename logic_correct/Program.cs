295ff8f82c85268518e38b716ec19908dc35991e
skeletonssnot <mclark13245@yahoo.com>
2026-02-25 18:12:43 -0500

    change push

---
+++ logic_correct/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logic_correct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+            
+        {
+            Console.WriteLine("thx gail!");
+        }
+    }
+}
