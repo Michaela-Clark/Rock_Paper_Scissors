c4c658a52c2246548cea109f961371b9ce20ed4f
skeletonssnot <mclark13245@yahoo.com>
2026-02-25 23:09:08 -0500

    finished logic

--- logic_correct/Program.cs
+++ logic_correct/Program.cs
@@ -5,14 +5,47 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace logic_correct
+
+
+/*
+* CS2 – Lab 6.1
+* Author: Michaela Clark
+* Date: 02/24/2026
+* Description: This class applies logic to the classes created in the final product. 
+*/
 {
-    internal class Program
+    public class Logic
+         {
+        public string DetermineWinner(Choice player, Choice computer)
+        {
+            if (player == computer)
+                return "Tie!";
+
+            if ((player == Choice.Rock && computer == Choice.Scissors) ||
+                (player == Choice.Paper && computer == Choice.Rock) ||
+                (player == Choice.Scissors && computer == Choice.Paper))
+            {
+                return "Player Wins!";
+            }
+
+            return "Computer Wins!";
+        }
+    }
+    class Program  ///Causes the program to run
     {
         static void Main(string[] args)
-
-            
         {
-            Console.WriteLine("thx gail!");
+            Player player = new Player();
+            ComputerPlayer computer = new ComputerPlayer();
+            Logic logic = new Logic();
+
+            Choice playerChoice = player.GetChoice();
+            Choice computerChoice = computer.GenerateChoice();
+
+            Console.WriteLine($"Computer chose: {computerChoice}");
+            Console.WriteLine(logic.DetermineWinner(playerChoice, computerChoice));
+
+            Console.ReadLine();
         }
     }
 }
